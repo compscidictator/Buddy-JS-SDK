fb794688795222fa73e392c1f37b5bf4209fb5c7
Erik Kerber <erik.kerber@gmail.com>
2013-10-02 22:44:03 -0500

    Unsubscribe from KVO on dealloc.

--- Buddy-iOS-SDK/Lib/BuddySDK/Internal/BuddyBase.m
+++ Buddy-iOS-SDK/Lib/BuddySDK/Internal/BuddyBase.m
@@ -11,6 +11,7 @@
 @interface BuddyBase()
 
 @property (nonatomic, readwrite, assign) BOOL isDirty;
+@property (nonatomic, strong) NSMutableArray *keyPaths;
 
 @end
 
@@ -21,6 +22,7 @@
     self = [super init];
     if(self)
     {
+        self.keyPaths = [NSMutableArray array];
         [self registerProperty:@"created"];
         [self registerProperty:@"lastModified"];
         [self registerProperty:@"tag"];
@@ -35,6 +37,7 @@
     self = [super init];
     if(self)
     {
+        self.keyPaths = [NSMutableArray array];
         [self registerProperty:@"created"];
         [self registerProperty:@"lastModified"];
         [self registerProperty:@"tag"];
@@ -46,7 +49,17 @@
 
 -(void)registerProperty:(NSString *)propertyName
 {
+    [self.keyPaths addObject:propertyName];
+    
     [self addObserver:self forKeyPath:propertyName options:NSKeyValueObservingOptionNew context:NULL];
+}
+
+-(void)dealloc
+{
+    for(NSString *keypath in self.keyPaths)
+    {
+        [self removeObserver:self forKeyPath:keypath];
+    }
 }
 
 -(void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary *)change context:(void *)context
