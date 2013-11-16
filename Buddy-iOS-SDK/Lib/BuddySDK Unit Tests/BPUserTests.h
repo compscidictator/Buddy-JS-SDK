3da560e0327f45056b9150c909ebfdd3a67b2ccd
Erik Kerber <erik.kerber@gmail.com>
2013-11-16 00:14:38 -0600

    Set up unit tests.

---
+++ Buddy-iOS-SDK/Lib/BuddySDK Unit Tests/BPUserTests.h
@@ -0,0 +1,13 @@
+//
+//  BPUserTests.h
+//  BuddySDK
+//
+//  Created by Erik Kerber on 11/15/13.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface BPUserTests : SenTestCase
+
+@end
---
+++ Buddy-iOS-SDK/Lib/BuddySDK Unit Tests/BPUserTests.m
@@ -0,0 +1,28 @@
+//
+//  BPUserTests.m
+//  BuddySDK
+//
+//  Created by Erik Kerber on 11/15/13.
+//
+//
+
+#import "BPUserTests.h"
+#import "Buddy.h"
+
+@implementation BPUserTests
+
+-(void)setUp
+{
+    [super setUp];
+}
+
+-(void)tearDown
+{
+    [super tearDown];
+}
+
+-(void)testInit
+{
+    //BPUser *user = [BPUser new];
+}
+@end
