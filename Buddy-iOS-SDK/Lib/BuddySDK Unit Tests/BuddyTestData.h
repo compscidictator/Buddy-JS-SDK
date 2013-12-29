cf8b15313d6103fed2875efb74b86cf676cf1c7d
Erik Kerber <erik@buddy.com>
2013-12-28 20:40:24 -0600

    Add Kiwi project references.

--- Buddy-iOS-SDK/Lib/BuddySDK Unit Tests/BuddyTestData.h
+++ Buddy-iOS-SDK/Lib/BuddySDK Unit Tests/BuddyTestData.h
@@ -1,5 +1,5 @@
 #define APP_NAME @"aaaac.baaaQ7FAWg"
 #define APP_KEY @"A941F46E-120E-4F71-A21E-03A0094FEAFF"
 
-#define TEST_USERNAME @"erik13"
+#define TEST_USERNAME @"erik15"
 #define TEST_PASSWORD @"password"
