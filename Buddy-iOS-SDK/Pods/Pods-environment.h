9a67b4f6581b21cd07a9d15468e8bf2689ecb9b7
Erik Kerber <erik@buddy.com>
2013-12-29 20:58:11 -0600

    Migrate ObjetiveSugar dependency to CocoaPods.

--- Buddy-iOS-SDK/Pods/Pods-environment.h
+++ Buddy-iOS-SDK/Pods/Pods-environment.h
@@ -48,3 +48,9 @@
 #define COCOAPODS_VERSION_MINOR_AFNetworking_UIKit 0
 #define COCOAPODS_VERSION_PATCH_AFNetworking_UIKit 3
 
+// ObjectiveSugar
+#define COCOAPODS_POD_AVAILABLE_ObjectiveSugar
+#define COCOAPODS_VERSION_MAJOR_ObjectiveSugar 1
+#define COCOAPODS_VERSION_MINOR_ObjectiveSugar 1
+#define COCOAPODS_VERSION_PATCH_ObjectiveSugar 0
+
