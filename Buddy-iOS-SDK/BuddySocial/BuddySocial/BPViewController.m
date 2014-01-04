960851a4aa728f9fdff9486036a11e350ce30d09
Erik Kerber <erik@buddy.com>
2014-01-03 19:39:00 -0600

    Social testing/demo project.

---
+++ Buddy-iOS-SDK/BuddySocial/BuddySocial/BPViewController.m
@@ -0,0 +1,29 @@
+//
+//  BPViewController.m
+//  BuddySocial
+//
+//  Created by Erik Kerber on 1/3/14.
+//  Copyright (c) 2014 Buddy. All rights reserved.
+//
+
+#import "BPViewController.h"
+
+@interface BPViewController ()
+
+@end
+
+@implementation BPViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
