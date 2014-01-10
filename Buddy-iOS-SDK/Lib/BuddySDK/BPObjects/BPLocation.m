7f6ca3903c3a2788714d42ffd6fe226664a3bf17
Erik Kerber <erik@buddy.com>
2014-01-09 18:35:39 -0600

    BPLocation object and properties.

---
+++ Buddy-iOS-SDK/Lib/BuddySDK/BPObjects/BPLocation.m
@@ -0,0 +1,40 @@
+//
+//  BPLocation.m
+//  BuddySDK
+//
+//  Created by Erik Kerber on 1/9/14.
+//
+//
+
+#import "BPLocation.h"
+#import "BuddyObject+Private.h"
+
+@implementation BPLocation
+
+- (instancetype)initBuddy{
+    self = [super initBuddy];
+    if(self)
+    {
+        [self registerProperty:@selector(name)];
+        [self registerProperty:@selector(description)];
+        [self registerProperty:@selector(streetAddress)];
+        [self registerProperty:@selector(city)];
+        [self registerProperty:@selector(state)];
+        [self registerProperty:@selector(country)];
+        [self registerProperty:@selector(zipcode)];
+        [self registerProperty:@selector(fax)];
+        [self registerProperty:@selector(phone)];
+        [self registerProperty:@selector(website)];
+        [self registerProperty:@selector(categoryId)];
+        [self registerProperty:@selector(distance)];
+    }
+    return self;
+}
+
+NSString *location = @"location";
++ (NSString *)requestPath
+{
+    return location;
+}
+
+@end
