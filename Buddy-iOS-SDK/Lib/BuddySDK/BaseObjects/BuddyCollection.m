4298d69072f5a8fac1880977132f3046aff91e2b
Erik Kerber <erik@buddy.com>
2014-01-24 08:33:13 -0600

    Use the preferred client in BuddyCollection.

--- Buddy-iOS-SDK/Lib/BuddySDK/BaseObjects/BuddyCollection.m
+++ Buddy-iOS-SDK/Lib/BuddySDK/BaseObjects/BuddyCollection.m
@@ -36,7 +36,7 @@
 
 -(void)search:(NSDictionary *)searchParmeters callback:(BuddyCollectionCallback)callback
 {
-    [[[BPClient defaultClient] restService] GET:[[self type] requestPath] parameters:searchParmeters callback:^(id json, NSError *error) {
+    [[self.client restService] GET:[[self type] requestPath] parameters:searchParmeters callback:^(id json, NSError *error) {
         NSArray *results = [json[@"pageResults"] map:^id(id object) {
             return [[self.type alloc] initBuddyWithResponse:object andClient:self.client];
         }];
@@ -50,7 +50,7 @@
                           [[self type] requestPath],
                           identifier];
     
-    [[[BPClient defaultClient] restService] GET:resource parameters:nil callback:^(id json, NSError *error) {
+    [[self.client restService] GET:resource parameters:nil callback:^(id json, NSError *error) {
         id buddyObject = [[self.type alloc] initBuddyWithResponse:json andClient:self.client];
         callback ? callback(buddyObject, error) : nil;
     }];
