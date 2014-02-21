2033048ea102790bc51dae57493e5255f94d78af
Nick Ambrose <nick@buddy.com>
2014-02-21 13:50:07 -0800

    Remove .NET and iOS

--- Buddy-DotNet-SDK/src/Properties/AssemblyInfoNet.cs
+++
@@ -1,3 +0,0 @@
-﻿
-[assembly: System.Reflection.AssemblyTitle("Buddy Platform Client SDK for .NET Framework")]
-[assembly: System.Reflection.AssemblyDescription("Buddy Platform Client SDK for .NET Framework")]
--- Buddy-DotNet-SDK/src/Properties/AssemblyInfoWp8.cs
+++
@@ -1,3 +0,0 @@
-﻿
-[assembly: System.Reflection.AssemblyTitle("Buddy Platform Client SDK for Windows Phone 8")]
-[assembly: System.Reflection.AssemblyDescription("Buddy Platform Client SDK for Windows Phone 8")]
